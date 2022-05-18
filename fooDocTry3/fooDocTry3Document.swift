42e3314000d7c1b82dc696022ca1662383c8b6c5
Allen King <allen@a-king.us>
2022-05-18 15:45:30 -0400

    adding SceneKit

--- fooDocTry3/fooDocTry3Document.swift
+++ fooDocTry3/fooDocTry3Document.swift
@@ -6,34 +6,36 @@
 //
 
 import SwiftUI
+import SceneKit
 import UniformTypeIdentifiers
 
 extension UTType {
-    static var exampleText: UTType {
-        UTType(importedAs: "com.example.plain-text")
-    }
+	static var exampleText: UTType {
+		UTType(importedAs: "com.example.plain-text")
+	}
 }
 
 struct fooDocTry3Document: FileDocument {
-    var text: String
+	var text: String
+//	var scene: SCNSacene?
 
-    init(text: String = "Hello, world!") {
-        self.text = text
-    }
+	init(text: String = "Hello, world!") {
+		self.text = text
+	}
 
-    static var readableContentTypes: [UTType] { [.exampleText] }
+	static var readableContentTypes: [UTType] { [.exampleText] }
 
-    init(configuration: ReadConfiguration) throws {
-        guard let data = configuration.file.regularFileContents,
-              let string = String(data: data, encoding: .utf8)
-        else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        text = string
-    }
-    
-    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        let data = text.data(using: .utf8)!
-        return .init(regularFileWithContents: data)
-    }
+	init(configuration: ReadConfiguration) throws {
+		guard let data = configuration.file.regularFileContents,
+			  let string = String(data: data, encoding: .utf8)
+		else {
+			throw CocoaError(.fileReadCorruptFile)
+		}
+		text = string
+	}
+	
+	func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+		let data = text.data(using: .utf8)!
+		return .init(regularFileWithContents: data)
+	}
 }
